dd207ac7319606af53f82048bc5fb45e8bb924e2
Jaid <jaid.jsx@gmail.com>
2020-10-10 11:18:34 +0200

    Added tests
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -4,12 +4,27 @@ import path from "path"
 
 const main = path.resolve(process.env.MAIN)
 
-it("should run", () => coffee.fork(main)
+it("should open for current directory", () => coffee.fork(main, ["--dry"])
   .expect("code", 0)
-  .expect("stdout", /^2\d{3}\n$/)
+  .expect("stdout", /https:\/\/github.com\/jaid\/open-repo-cli/si)
   .end())
 
-it("should run with options", () => coffee.fork(main, ["--no-newline"])
+it("should open without specified owner", () => coffee.fork(main, ["--dry", "--github-user", "Jaid", "sure-array"])
   .expect("code", 0)
-  .expect("stdout", /^2\d{3}$/)
+  .expect("stdout", /https:\/\/github.com\/jaid\/sure-array/si)
+  .end())
+
+it("should open any repo", () => coffee.fork(main, ["--dry", "webpack/tapable"])
+  .expect("code", 0)
+  .expect("stdout", /https:\/\/github.com\/webpack\/tapable/s)
+  .end())
+
+it("should open by npm name", () => coffee.fork(main, ["npm", "--dry", "chalk"])
+  .expect("code", 0)
+  .expect("stdout", /https:\/\/github.com\/chalk\/chalk/s)
+  .end())
+
+it("should open by npm name with scope", () => coffee.fork(main, ["npm", "--dry", "@babel/core"])
+  .expect("code", 0)
+  .expect("stdout", /https:\/\/github.com\/babel\/babel/s)
   .end())
